9da4340ac5baa99a79f15dffa884a94cfb25d658
Ryan Manchester <ryanm17@gmail.com>
2020-02-17 23:58:15 -0500

    first commit with Item model and db

---
+++ db/migrate/20200218043332_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.string :size
+      t.string :image_url
+      t.float :price
+      t.boolean :sold
+
+      t.timestamps
+    end
+  end
+end
