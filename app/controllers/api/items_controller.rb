6e31aacae94a45a3cae89272ec03616431010371
github username <github email address>
2020-02-18 14:25:03 -0500

    added CRUD actions to items controller

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,14 +1,38 @@
 class Api::ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :update, :destroy]
+
   def index
+    items = Item.all
+    render json: items
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json: {error: "Please make sure all fields are filled"}
+    end
   end
 
   def show
+    render json: @item
   end
 
   def update
   end
 
   def destroy
+    @item.destroy
+  end
+
+  private
+  def find_item
+    @item = Item.find_by(id: params[:id])
+  end
+  
+  def item_params
+    params.require(:item).permit(:name, :category_id, :description, :size, :price, :image_url, :sold)
   end
 
 end
