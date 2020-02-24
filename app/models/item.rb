1e897702954dc7d6f44835fe713ac22b59da888d
github username <github email address>
2020-02-23 19:33:12 -0500

    created order models and associations to users and items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :seller
+  has_many :order_items
   #validates :name, :size, :description, :price, :image_url, presence: true
 end
