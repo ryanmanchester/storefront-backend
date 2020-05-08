3f4d211bb4da2537c9428f66a550cda18eb9ef8f
github username <github email address>
2020-05-08 17:54:46 -0400

    sellers controller and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+    resources :sellers, only: [:create, :show, :destroy]
     resources :categories do
       resources :items
     end
