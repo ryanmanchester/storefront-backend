2dbd7663ac5d609b15bbd29e604b3201adfb44c4
Ryan Manchester <ryanm17@gmail.com>
2020-05-17 19:41:06 -0400

    cookies enabled in app.rb and CORS

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,10 +3,21 @@ class Api::V1::SessionsController < ApplicationController
     seller = Seller.find_by(name: params[:session][:name])
     if seller && seller.authenticate(params[:session][:password])
       session[:seller_id] = seller.id
+      byebug
       render json: seller
     else
       render json: {
         error: "Seller Not Found"
+      }
+    end
+  end
+
+  def get_current_seller
+    if logged_in?
+      render json: current_seller
+    else
+      render json: {
+        error: "Please Log In"
       }
     end
   end
