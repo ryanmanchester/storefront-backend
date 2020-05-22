d330b77adc86942ea1cc2bcecbc3f048560a34c5
github username <github email address>
2020-05-22 18:35:41 -0400

    logout route and controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post '/api/v1/sellers/login', to: 'api/v1/sessions#create'
+  delete '/api/v1/sellers/logout', to: 'api/v1/sessions#destroy'
   get '/api/v1/sellers/get_current_seller', to: 'api/v1/sessions#get_current_seller'
   namespace :api do
     namespace :v1 do
