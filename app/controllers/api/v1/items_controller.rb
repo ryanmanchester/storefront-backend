4dc403836b4784d8dcdf5244d15eabce24ad8a34
Ryan Manchester <ryanm17@gmail.com>
2021-01-03 13:38:03 -0500

    gets rough checkout feature working

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -64,6 +64,18 @@ class Api::V1::ItemsController < ApplicationController
     }
   end
 
+  def place_order
+    cart = session[:cart]
+    order = cart.map {|item| Item.find(item[:id])}
+    if order.each {|item| item.destroy}
+      clear_cart
+    else
+      render json: {
+        message: "An error has occured with your order. Please try again."
+      }
+    end
+  end
+
   private
 
   def set_item
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post'/api/v1/orders', to: 'api/v/orders#create'
   delete '/api/v1/sellers/logout', to: 'api/v1/sessions#destroy'
   delete '/api/v1/clear_cart', to: 'api/v1/items#clear_cart'
+  delete '/api/v1/place_order', to: 'api/v1/items#place_order'
   get '/api/v1/sellers/get_current_seller', to: 'api/v1/sessions#get_current_seller'
   get '/api/v1/items/get_current_cart', to: 'api/v1/items#get_current_cart'
   namespace :api do
